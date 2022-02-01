6a1617d64191e298fc29efbdbc6f6daa674bec3d
angelo <angelogustavo157@gmail.com>
2022-01-31 21:00:15 -0300

    feat: adicionando classe Juros

---
+++ src/main/java/Emprestimo/Juros.java
@@ -0,0 +1,41 @@
+package Cliente;
+
+public class Juros {
+    private Integer prazoMeses;
+    private Double montante;
+    private Double taxaJuros;
+    private Double taxaImposto;
+
+    public Juros(Integer prazoMeses, Double montante, Double taxaJuros, Double taxaImposto){
+        this.prazoMeses = prazoMeses;
+        this.montante = montante;
+        this.taxaJuros = taxaJuros;
+        this.taxaImposto = taxaImposto;
+    }
+
+    public Integer getPrazoMeses(){
+        return prazoMeses;
+    }
+    public Double getMontante(){
+        return montante;
+    }
+    public Double getTaxaJuros(){
+        return taxaJuros;
+    }
+    public Double getTaxaImposto(){
+        return taxaImposto;
+    }
+    
+    public void setPrazoMeses(Integer prazoMeses){
+        this.prazoMeses = prazoMeses;
+    }
+    public void setMontante(Double montante){
+        this.montante = montante;
+    }
+    public void setTaxaJuros(Double taxaJuros){
+        this.taxaJuros = taxaJuros;
+    }
+    public void setTaxaImposto(Double taxaImposto){
+        this.taxaImposto = taxaImposto;
+    }
+}
