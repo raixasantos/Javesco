b412c06907c3e9a4c707b222cc0fb8820126b3a3
Raíssa <raixapsantos@gmail.com>
2022-02-10 21:59:14 -0300

    feat: adicionando classe Emprestimo

---
+++ src/main/java/Emprestimo/Emprestimo.java
@@ -0,0 +1,33 @@
+package Emprestimo;
+
+import java.util.Date;
+
+public class Emprestimo {
+    private Date dataInicio;
+    private Date dataFim;
+    private Double valorTotal;
+    private Juros juros;
+
+    public Emprestimo(Date dataInicio, Date dataFim, Double valorTotal, Juros juros_){
+        this.dataInicio = dataInicio;
+        this.dataFim = dataFim;
+        this.valorTotal = valorTotal;
+        this.juros = juros;
+    }
+
+    public Date getDataInicio() {
+        return dataInicio;
+    }
+
+    public Date getDataFim() {
+        return dataFim;
+    }
+
+    public Double getValorTotal() {
+        return valorTotal;
+    }
+
+    public Juros getJuros() {
+        return juros;
+    }
+}
