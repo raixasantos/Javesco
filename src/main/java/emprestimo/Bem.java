f45c0677b697ccb8109c5e5265dd0dce57e1ae01
angelo <angelogustavo157@gmail.com>
2022-02-12 14:29:40 -0300

    feat: adicionando tipo

--- src/main/java/emprestimo/Bem.java
+++ src/main/java/emprestimo/Bem.java
@@ -1,12 +1,22 @@
 package emprestimo;
 
 public enum Bem {
-    IMOVEL,
-    VEICULAR;
+    IMOVEL("imobiliário"),
+    VEICULO("veicular");
 
     protected Double valor;
+    protected String tipoFinanciamento;
+
+    Bem(String tipoFinanciamento){
+        this.tipoFinanciamento = tipoFinanciamento;
+    }
+
+    public String getTipoFinanciamento(){
+        return tipoFinanciamento;
+    }
 
     public Double getValor() {
         return valor;
     }
+
 }
