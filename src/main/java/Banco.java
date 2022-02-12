24f217395bb2c05bab2a3fdb26ced97735ec271a
silviafds <silviafernandes10@outlook.com.br>
2022-02-12 19:38:30 -0300

    feat: Documentação da classe Banco

--- src/main/java/Banco.java
+++ src/main/java/Banco.java
@@ -1,5 +1,8 @@
 import gui.InterfaceGrafica;
 
+/**
+ * Classe que principal que inicia os procedimentos do código
+ */
 public class Banco {
     public static void main(String[] args) {
         InterfaceGrafica.menu();
