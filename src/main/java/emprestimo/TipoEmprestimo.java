01cda59795404e3848371230f604fb679ef01a5c
silviafds <silviafernandes10@outlook.com.br>
2022-02-12 19:33:18 -0300

    feat: Doumentação da classe TipoEmprestimo

--- src/main/java/emprestimo/TipoEmprestimo.java
+++ src/main/java/emprestimo/TipoEmprestimo.java
@@ -1,5 +1,8 @@
 package emprestimo;
 
+/**
+ * Enum que possui os tipos de empréstimo
+ */
 public enum TipoEmprestimo {
     CREDITO("Empréstimo de crédito", 0.25),
     IMOVEL("Financiamento imobiliário", 0.1),
@@ -8,15 +11,28 @@ public enum TipoEmprestimo {
     protected Double taxa;
     protected String tipoFinanciamento;
 
+    /**
+     *
+     * @param tipoFinanciamento Tipo do financiamento escolhido pelo cliente
+     * @param taxa Taxa de juro utilizada para calcular o empréstimo solicitado
+     */
     TipoEmprestimo(String tipoFinanciamento, Double taxa) {
         this.tipoFinanciamento = tipoFinanciamento;
         this.taxa = taxa;
     }
 
+    /**
+     *
+     * @return retorna o tipo de financiamento escolhido
+     */
     public String getTipoFinanciamento() {
         return tipoFinanciamento;
     }
 
+    /**
+     *
+     * @return retorna o valor da taxa utilizada no cálculo
+     */
     public Double getTaxa() {
         return taxa;
     }
